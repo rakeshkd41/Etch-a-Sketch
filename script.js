0a3c5ea532f43d5dcef3fd5af854d880c087b649
rakesh <rakeshkdmb41@gmail.com>
2024-08-24 23:16:49 +0530

    add nested for loop to create 16X16 grid

--- script.js
+++ script.js
@@ -8,7 +8,21 @@ const rainbow = document.querySelector('.rainbow')
 
 
 function etchASketch(gridSize) {
+  for (let i = 1; i <= gridSize; i++) {
+    const divi = document.createElement('div')
+    divi.style.cssText = 
+    'flex: 1; display: flex; flex-direction: column'
+    mainContainer.appendChild(divi)
   
+    for(let j = 1; j <= gridSize; j++) {
+      const divij = document.createElement('div')
+      divij.style.cssText = 
+      'border: 1px solid black; flex: 1'
+      divi.appendChild(divij)
+  
+      
+    }
+  }
 }
 
 etchASketch(16);
