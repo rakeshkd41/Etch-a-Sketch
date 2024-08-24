a18a37527aa110ceb05e086b8e374d3f459a2616
rakesh <rakeshkdmb41@gmail.com>
2024-08-24 23:10:20 +0530

    select all buttons and main-container

---
+++ script.js
@@ -0,0 +1,7 @@
+const mainContainer = document.querySelector('.main-container')
+const setGridSize = document.querySelector('.grid-size')
+
+const eraser= document.querySelector('.eraser')
+const red = document.querySelector('.red')
+const blue = document.querySelector('.blue')
+const rainbow = document.querySelector('.rainbow')
