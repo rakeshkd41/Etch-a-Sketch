093efed692e74aeb84dfac4a71d85a3a89619e1a
rakesh <rakeshkdmb41@gmail.com>
2024-08-24 23:22:54 +0530

    add getRandomColor() function to change the color of rainbow button randomly on each other divs

--- script.js
+++ script.js
@@ -34,4 +34,13 @@ function etchASketch(gridSize) {
   }
 }
 
+function getRandomColor() {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 etchASketch(16);
